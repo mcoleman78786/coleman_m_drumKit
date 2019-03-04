13fb5dbaf876f57fc606c7d4e9c938dca1704db2
Mallory Coleman <mallorycoleman@Mallorys-Air.fc.ca>
2019-03-04 14:52:10 -0500

    updated main.js and add sound

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 (() => {
 	console.log("fired! read to jam");
 
+	function playDrumKitSound(event) {
+	    // debugger;
+	    // Select the corresponding audio element and make it play
+	    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	    console.log(audio);
+
+	    audio.play();
+	}
+
+	window.addEventListener("keydown", playDrumKitSound);
 })();
