c9ec163558768950602e36a54a0babe9b3637aee
Mallory Coleman <mallorycoleman@Mallorys-Air.fc.ca>
2019-03-04 15:14:06 -0500

    debugged

--- js/main.js
+++ js/main.js
@@ -5,8 +5,16 @@
 	    // debugger;
 	    // Select the corresponding audio element and make it play
 	    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-	    console.log(audio);
+	    // console.log(audio);
+	    // 
+	   // Debugging / error handling
+	   // if we don't have a matching audio element, then kill the funciton
+	   // ! is a "not" operator or a bang
+	   if (!audio) { return; }  // return stops code execution
 
+
+	   	//  Rewind audio on every click and make it play
+	    audio.currentTime = 0;
 	    audio.play();
 	}
 
